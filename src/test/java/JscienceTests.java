322bcef9b1a5d96cb3ca96ccf49dc3a4e34eeef7
Igor Popov <igor.popov89@gmail.com>
2013-08-10 09:13:27 +0300

    Add initial test for conversions
    Signed-off-by: Igor Popov <igor.popov89@gmail.com>

---
+++ src/test/java/JscienceTests.java
@@ -0,0 +1,37 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.measure.unit.NonSI;
+import javax.measure.unit.Unit;
+import javax.measure.unit.UnitFormat;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Check the SI, NonSI and the UnitFormat classes (from jscience)
+ * for all the available units (at the bottom of the class)
+ */
+public class JscienceTests {
+    @Before
+    public void addMoreAliases() throws Exception {
+        UnitFormat unitFormat = UnitFormat.getInstance();
+        unitFormat.alias(NonSI.NAUTICAL_MILE, "nm");
+    }
+
+    private double convert(double value, String fromUnit, String toUnit) {
+        return Unit.valueOf(fromUnit)
+                .getConverterTo(Unit.valueOf(toUnit))
+                .convert(value);
+    }
+
+    private void assertConversion(double fromValue, String fromUnit,
+                                  double toValue, String toUnit) {
+        double convertedValue = convert(fromValue, fromUnit, toUnit);
+        assertEquals(toValue, convertedValue, .0001);
+    }
+
+    @Test
+    public void canConvert() {
+        assertConversion(5.00, "nm", 9.26, "km");
+    }
+}
